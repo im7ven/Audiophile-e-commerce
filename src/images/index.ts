d45b1ecb7a839c24e67b8d6ebc552e91601db334
Justin <alexanderrr416@gmail.com>
2023-08-26 17:57:53 -0400

    Creating index for images.

---
+++ src/images/index.ts
@@ -0,0 +1,15 @@
+import headphoneCategoryL from "./category-thumbnail-headphones-L.webp";
+import headphoneCategoryS from "./category-thumbnail-headphones-S.webp";
+import speakerCategoryL from "./category-thumbnail-speakers-L.webp";
+import speakerCategoryS from "./category-thumbnail-speakers-S.webp";
+import earphoneCategoryL from "./category-thumbnail-earphones-L.webp";
+import earphoneCategoryS from "./category-thumbnail-earphones-S.webp";
+
+export {
+  headphoneCategoryL,
+  headphoneCategoryS,
+  speakerCategoryL,
+  speakerCategoryS,
+  earphoneCategoryL,
+  earphoneCategoryS,
+};
